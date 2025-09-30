c730638fee3f54e45262b09268b82da6514251a3
victor0710s <s.vih0710@gmail.com>
2025-09-30 20:00:18 -0300

    feat: aprendendo loops

---
+++ JS - Iniciante/Aula07/script.js
@@ -0,0 +1,14 @@
+// LOOPS
+
+// while => Estrutura de repetição que executa um bloco de código enquanto uma condição for verdadeira
+// var x = 0;
+
+// while (x < 10) {
+//   document.write("<br> O valor de x é: " + x);
+//   x++; // Incrementa o valor de x em 1 a cada iteração
+// }
+
+// for => Estrutura de repetição que executa um bloco de código um número específico de vezes
+for (a = 0; a < 5; a++) {
+  document.write("<br> O valor de a é: " + a);
+}
