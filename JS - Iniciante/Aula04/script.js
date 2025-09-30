0493f060489db9175d844e63359da7474d4efa59
victor0710s <s.vih0710@gmail.com>
2025-09-30 14:41:38 -0300

    feat: comandos básicos de entrada e inserindo elementos no document

---
+++ JS - Iniciante/Aula04/script.js
@@ -0,0 +1,10 @@
+// Comandos de entrada
+
+var nome = prompt("Qual é o seu nome?");
+// var sobrenome = prompt("Qual é o seu sobrenome?");
+
+// console.log("Olá " + nome + " " + sobrenome);
+
+document.write("<h1> Bem vindo " + nome + "</h1>");
+
+document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='Foto do steve jobs'/>")
