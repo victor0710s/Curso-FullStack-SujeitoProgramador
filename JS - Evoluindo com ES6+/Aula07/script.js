9da96ef19eb23746a943d412899a887338a4d53b
victor0710s <s.vih0710@gmail.com>
2025-10-03 20:32:20 -0300

    feat: includes, endsWith e startsWith

---
+++ JS - Evoluindo com ES6+/Aula07/script.js
@@ -0,0 +1,17 @@
+//* Includes
+
+// let nome = ["Victor","Jose","Luis"]
+// console.log(nome.includes("Jose")); // retorna true
+
+// if (nome.includes("Victor")) { 
+//   console.log("Está na lista")
+// } else {
+//   console.log("Não está na lista");
+// }
+
+//* startsWith e endsWith
+
+let nome = "Victor";
+console.log(nome.startsWith("V"));
+console.log(nome.endsWith("s"));
+
