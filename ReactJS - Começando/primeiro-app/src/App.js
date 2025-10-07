eb7c0d661279ede07ca8cc64ee17bd738f44c8e2
victor0710s <s.vih0710@gmail.com>
2025-10-07 16:28:57 -0300

    feat: usando o useState com arrays

--- ReactJS - Começando/primeiro-app/src/App.js
+++ ReactJS - Começando/primeiro-app/src/App.js
@@ -2,22 +2,17 @@ import { useState } from 'react';
 
 // Criando um componente
 function App() {
-
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [idade, setIdade] = useState('');
-
-  const [user, setUser] = useState({});
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Estudar React',
+    'Pagar contas'
+  ]);
 
   function handleRegister(e) {
     e.preventDefault(); // Previnir o comportamento padrão do formulário
 
-    alert("Usuario cadastrado com sucesso!");
-    setUser({
-      nome: nome,
-      email: email,
-      idade: idade
-    });
+    setTarefas([...tarefas, input]); 
+    setInput(''); // Limpar o input
   }
 
   return( //retornando o JSX
@@ -27,23 +22,9 @@ function App() {
       <form onSubmit={handleRegister}>
         <label>Nome: </label><br />
         <input 
-          placeholder='Digite seu nome'
-          value={nome} // Valor recebido no input
-          onChange={ (e) => setNome(e.target.value)}  // Função disparada quando o valor do input mudar
-        /><br />
-
-        <label>Email: </label><br />
-        <input 
-          placeholder='Digite seu email' 
-          value={email} // Valor recebido no input
-          onChange={ (e) => setEmail(e.target.value)}
-        /><br />
-
-        <label>Idade: </label><br />
-        <input
-          placeholder='Digite sua idade' 
-          value={idade} // Valor recebido no input
-          onChange={ (e) => setIdade(e.target.value)}
+          placeholder='Digite uma tarefa'
+          value={input} // Valor recebido no input
+          onChange={ (e) => setInput(e.target.value)}  // Função disparada quando o valor do input mudar
         /><br />
 
         <button type='submit'>Registrar</button>
@@ -51,11 +32,11 @@ function App() {
 
       <br /><br />
 
-      <div>
-        <span>Bem vindo: {user.nome}</span><br />
-        <span>Email: {user.email}</span><br />
-        <span>Idade: {user.idade}</span><br />
-      </div>
+      <ul>
+        {tarefas.map( tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))} 
+      </ul>
     </div>
   );
 }
