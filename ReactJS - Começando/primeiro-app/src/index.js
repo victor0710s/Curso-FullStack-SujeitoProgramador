c8f8a1ec6fb2880c22941bbb631c25b8b8d71b92
victor0710s <s.vih0710@gmail.com>
2025-10-08 12:30:59 -0300

    feat: praticando requisicoes http

--- ReactJS - Começando/primeiro-app/src/index.js
+++ ReactJS - Começando/primeiro-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+import App from './Reques';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( // renderiza um componente react na tela, chamado App
