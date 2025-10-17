02c9366af773d8ddf986fe53eaf9e1b9bd2e00f9
victor0710s <s.vih0710@gmail.com>
2025-10-17 11:37:40 -0300

    feat: detalhes do filme

--- ReactJS - Prime Flix/prime-flix/src/pages/Filme/index.js
+++ ReactJS - Prime Flix/prime-flix/src/pages/Filme/index.js
@@ -1,7 +1,67 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import './filme.css';
+
 function Filme() {
+  const { id } = useParams();
+
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme(){
+      await api.get(`/movie/${id}`, {
+        params: {
+          api_key: '38ba0474025f904a2ddcedf4b29f7891',
+          language: 'pt-BR',
+        }
+      })
+      .then((response) => {
+        setFilme(response.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        console.log("Filme não encontrado");
+      })
+
+    }
+
+    loadFilme();
+    return () => {
+      console.log("COMPONENTE DESMONTADO");
+    };
+  }, []);
+
+  if (loading) {
+    return(
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    )
+  }
+
   return(
-    <div>
-      <h1>BEM VINDO À PAGINA DETALHES DO FILME</h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>
+      
+      <h3>Sinpose</h3>
+      <span>{filme.overview}</span>
+
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+      <div className='area-buttons'>
+        <button>Salvar</button>
+        <button>
+          <a href='#'>
+            Trailer
+          </a>
+        </button>
+
+      </div>
     </div>
   )
 }
