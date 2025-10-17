aa66d0487bf028b74e93794f675483db41ecd075
victor0710s <s.vih0710@gmail.com>
2025-10-17 13:21:50 -0300

    feat: exclusao de filmes

--- ReactJS - Prime Flix/prime-flix/src/pages/Favoritos/index.js
+++ ReactJS - Prime Flix/prime-flix/src/pages/Favoritos/index.js
@@ -12,9 +12,20 @@ function Favoritos() {
     setFilmes(JSON.parse(minhaLista) || []);
   }, []);
 
+  function excluirFilme(id) {
+    let filtroFilmes = filmes.filter( (filme) => {
+      return (filme.id !== id)
+    })
+
+    setFilmes(filtroFilmes);
+    localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes));
+  }
+
   return(
     <div className="meus-filmes">
       <h1>Meus Filmes</h1>
+
+      { filmes.length === 0 && <span>Você não possui nenhum filme salvo :( </span>}
 
       <ul>
         { filmes.map( (filme) => {
@@ -24,7 +35,7 @@ function Favoritos() {
 
               <div>
                 <Link to={`/filme/${filme.id}`} >Ver detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={ () => excluirFilme(filme.id) }>Excluir</button>
               </div>
             </li>
           )
